df10d5fdd0b6323c2286e4f8e677109c5779ce3d
Brahms <yangkaiyuan@u.nus.edu>
2018-02-10 13:42:20 +0800

    rename transmaxcal, popen terminate and close window

--- computing_server.py
+++ computing_server.py
@@ -9,20 +9,19 @@ app.secret_key = 'C++Bear'
 # global variable to show computation status
 cal_proc = None
 
-@app.route('/octave_cal/')
-def octave_cal():
-    # process the js object from /jsontest
-    # request.get_json() converts the JSON object into Python data
-    # req_data = request.get_json()
-    # print(req_data)
+@app.route('/transmaxcal/')
+def transmaxcal():
+    # process input from /jsontest as GET in the URL queries
     print(request.args)
     DNA_length = request.args.get('DNA_length')
     force = request.args.get('force')
     torque = request.args.get('torque')
     max_mode = request.args.get('max_mode')
 
-    # subprocess.Popen() the Octave program
+    # call the global variable cal_proc
     global cal_proc
+
+    # subprocess run the computation
     cal_proc = subprocess.Popen([\
         "C:\Octave\Octave-4.2.1\\bin\octave-cli.exe", "--eval", \
         "main_DNA_force_torque_spectrum_new(%s,%s,%s,%s)" % (DNA_length, force, torque, max_mode)], \
@@ -31,8 +30,23 @@ def octave_cal():
 
     return jsonify(serverMsg = "Program Started")
 
+@app.route('/kill_transmaxcal/')
+def kill_transmaxcal():
+    # first call the global variable
+    global cal_proc
+    # detect if stopCal signal is present
+    print(request.args)
+    stop_cal = request.args.get('stopCal')
+    if stop_cal == "confirmed":
+        cal_proc.kill()
+        print("calculation cancelled by user")
+        return jsonify(confirmedkill = True)
+    return "kill the boy!"
+
 @app.route('/check_computation_status/')
 def check_computation_status():
+    # first call the global variable
+    global cal_proc
     # process pollServer from webpage
     # if cal_proc is not finished
     if cal_proc is None or cal_proc.poll() is None:
@@ -41,7 +55,8 @@ def check_computation_status():
         # non-block method (but .stdout.read() actually block...)
         outs = cal_proc.stdout.read().decode('ascii')
         print(outs)
-        return jsonify(computationStatus = True, result = outs)
+        finish_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        return jsonify(computationStatus = True, done_time = finish_time, result = outs)
 
 @app.route('/jsontest/')
 def jsontest():
@@ -64,7 +79,6 @@ def jsontest():
                            max_mode = max_mode
                            )
 
-
 @app.route('/jsonshowtime/')
 def jsonshowtime():
     time_json = strftime("%Y-%m-%d %H:%M:%S", localtime())
@@ -72,12 +86,3 @@ def jsonshowtime():
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 7717)
-
-"""
-for future reference: this actually worked
-    output, rubbish = subprocess.Popen([\
-        "C:\Octave\Octave-4.2.1\\bin\octave-cli.exe", "--eval", \
-        "OctaveSumTest(1,2)"], \
-        stdout=subprocess.PIPE, \
-        cwd='C:\\Users\LUMICKS\Desktop\FlaskServer').communicate()
-"""
