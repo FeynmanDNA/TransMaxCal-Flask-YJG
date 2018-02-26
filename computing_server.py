5b886e0f90430c3d7107e6ec6af589ddba7c3350
Brahms <yangkaiyuan@u.nus.edu>
2018-02-26 14:22:57 +0800

    threadtest forceArray flotplot

--- computing_server.py
+++ computing_server.py
@@ -3,7 +3,7 @@ from time import localtime, strftime
 import subprocess
 import sys
 
-# replace sys.stdout with some other stream like wrapper 
+# replace sys.stdout with some other stream like wrapper
 # which does a flush after every call.
 class Unbuffered(object):
    def __init__(self, stream):
@@ -21,92 +21,53 @@ sys.stdout = Unbuffered(sys.stdout)
 
 app = Flask(__name__)
 
-# global variable to show computation status
-cal_proc = None
-
-@app.route('/transmaxcal/')
-def transmaxcal():
-    # process input from /jsontest as GET in the URL queries
-    print(request.args)
-    DNA_length = request.args.get('DNA_length')
-    force = request.args.get('force')
-    torque = request.args.get('torque')
-    max_mode = request.args.get('max_mode')
-
-    # call the global variable cal_proc
-    global cal_proc
-
-    # subprocess run the computation
-    cal_proc = subprocess.Popen(["C:\\Users\\LUMICKS\\Desktop\\FlaskServer\\gppCMDversionFeb19th.exe", \
-            "%s" % (DNA_length), "%s" % (force), "%s" % (torque), "%s" % (max_mode)], \
-        stdout=subprocess.PIPE)
-
-    return jsonify(serverMsg = "Program Started", computationStarted = True)
-
-@app.route('/kill_transmaxcal/')
-def kill_transmaxcal():
-    # first call the global variable
-    global cal_proc
-    # detect if stopCal signal is present
-    print(request.args)
-    stop_cal = request.args.get('stopCal')
-    if stop_cal == "confirmed":
-        #if cal_proc: cal_proc.kill()
-        try:
-            cal_proc.kill()
-        except AttributeError:
-            # if connection between popup and server resumes
-            pass
-        finish_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
-        print("calculation cancelled by user @ ", finish_time)
-
-        # reset global cal_proc
-        cal_proc = None
-
-        return jsonify(confirmedkill = True, done_time = finish_time)
-
-    return "kill the boy!"
-
-@app.route('/check_computation_status/')
-def check_computation_status():
-    # first call the global variable
-    global cal_proc
-    # process pollServer from webpage
-    # if cal_proc is not finished
-    if cal_proc is None or cal_proc.poll() is None:
-        return jsonify(computationStatus = False)
-    else:
-        # non-block method (but .stdout.read() actually block...)
-        outs = cal_proc.stdout.read().decode('ascii')
-        print(outs)
-        finish_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
-
-        # reset global cal_proc
-        cal_proc = None
-
-        return jsonify(computationStatus = True, done_time = finish_time, result = outs)
-
 @app.route('/jsontest/')
 def jsontest():
-    # first check if there is already a transmaxcal process
-    global cal_proc
+    # record the request submit time
+    submit_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
 
-    if cal_proc is None:
-        pass
-    elif cal_proc.poll() is None:
-        # the transmaxcal subprocess is alive
-        return render_template('BlockFurReq.html')
+    # show submit time to stdout
+    print("@", submit_time)
 
     # print out the request headers like user-agent etc
+    print("==========request.headers received by Flask is:=========")
     print(request.headers)
-    # for user to click to see current server time
-    submit_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
 
     # process the URL input
     DNA_length = request.args.get('DNAlength')
-    force = request.args.get('force')
+    force_str = request.args.get('force').split(",")
     torque = request.args.get('torque')
     max_mode = request.args.get('maxMode')
+
+    # to prevent empty url queries
+    try:
+        int(DNA_length)
+        for i in force_str:
+            float(i)
+        float(torque)
+        int(max_mode)
+    except (TypeError, ValueError):
+        print("empty or invalid  url queries detected")
+        return "please input valid parameters"
+
+    # force_str is now list like ['1', '20', '199', '0.1']
+    force = [float(i) for i in force_str]
+    # force is an array [1.0, 20.0, 199.0, 0.1]
+
+    # server-side url queries validation
+    # first validate force array
+    for item in force:
+        if item <=0 or item>=200:
+            print("user is entering force values in url...")
+            return "please submit valid force values"
+
+    if (int(DNA_length) <= 0 or
+        float(torque) < -30 or
+        float(torque) > 50 or
+        int(max_mode) < 10 or
+        int(max_mode) >20):
+        print("user is entering the url queries in the pop up...")
+        return "please submit valid parameters"
 
     return render_template('TransMatCal.html',
                            submit_time = submit_time,
@@ -116,11 +77,36 @@ def jsontest():
                            max_mode = max_mode
                            )
 
+@app.route('/transmaxcal/')
+def transmaxcal():
+    # process input from /jsontest as GET in the URL queries
+    print(">>>>>>>>>Page now redirect to /transmaxcal")
+    print("request.args is:")
+    print(request.args)
+    DNA_length = request.args.get('DNA_length')
+    force = request.args.get('force').split(",")
+    torque = request.args.get('torque')
+    max_mode = request.args.get('max_mode')
+
+    # initiate outs as empty list
+    outs = []
+    # force is a list like ['1', '3.3', '5']
+    for i in force:
+        cal_proc = subprocess.Popen(["./artem_update.out", \
+                   "%s" % (DNA_length), "%s" % (i), "%s" % (torque), "%s" % (max_mode)], \
+                   stdout=subprocess.PIPE)
+        outs.append(cal_proc.stdout.read().decode('ascii'))
+        print("cal_proc now is ", cal_proc)
+
+    finish_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+    print("Now /transmaxcal/ returns the json back to page")
+    return jsonify(done_time = finish_time, result = outs)
+
 @app.route('/jsonshowtime/')
 def jsonshowtime():
     time_json = strftime("%Y-%m-%d %H:%M:%S", localtime())
     return jsonify(result = time_json)
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 7717)
+    app.run(host = "0.0.0.0", port = 7717, threaded=True)
 
